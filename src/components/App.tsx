b3e55997338462befb4915a718eb2a81222ef1ab
Marina Prushak <87096097+huruzka@users.noreply.github.com>
2025-09-02 00:26:03 +0300

    voteOptions

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import css from "../styles/App.module.css";
 import CafeInfo from "./CafeInfo";
-
+import VoteOptions from "./VoteOptions";
+import type { Votes, VoteType } from "../types/votes";
 
 const App = () => {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  // функція для голосування
+  const handleVote = (type: VoteType) => {
+    setVotes((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  };
+
+  // функція для скидання голосів
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
   return (
     <div className={css.app}>
       <CafeInfo />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
     </div>
   );
 };
